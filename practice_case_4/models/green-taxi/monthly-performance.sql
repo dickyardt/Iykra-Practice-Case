7164de031d1ad2adac6e541343af86af24b67e18
Dicky A <65923273+dickyardt@users.noreply.github.com>
2022-09-30 11:50:13 +0700

    Add files via upload

---
+++ practice_case_4/models/green-taxi/monthly-performance.sql
@@ -0,0 +1,12 @@
+with monthly_performance as (select  EXTRACT(MONTH FROM DATE(lpep_pickup_datetime))  AS month,
+        count(trip_distance) as total_pickup,
+        sum(trip_distance) as total_trip_distance,
+        avg(trip_distance) as average_trip_distance,
+        sum(passenger_count) as total_passenger,
+        avg(passenger_count) as average_passenger,
+        sum (total_amount) as total_amount,
+        avg(total_amount) as average_amount
+from {{ref('source-table')}}
+group by month
+order by month asc)
+select * from monthly_performance
---
+++ practice_case_4/models/green-taxi/source-table.sql
@@ -0,0 +1,25 @@
+with source_table as (select * from {{source('green','january')}}
+union all
+select * from {{source('green','february')}}
+union all
+select * from {{source('green','march')}}
+union all
+select * from {{source('green','april')}}
+union all
+select * from {{source('green','may')}}
+union all
+select * from {{source('green','june')}}
+union all
+select * from {{source('green','july')}}
+union all
+select * from {{source('green','agustus')}}
+union all
+select * from {{source('green','september')}}
+union all
+select * from {{source('green','october')}}
+union all
+select * from {{source('green','november')}}
+union all
+select * from {{source('green','december')}})
+
+select * from source_table
